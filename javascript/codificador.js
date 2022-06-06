cedd83c1e38ef2db1ae545df9777f254a2eb326a
Mikael Josias <mikaeljosias2002@gmail.com>
2022-06-06 20:02:58 -0300

    feat(decryptor): add the decryptor function

--- javascript/codificador.js
+++ javascript/codificador.js
@@ -1,5 +1,6 @@
 
 var btnCodificar = document.querySelector("#btnCodificar");
+var btnDecodificar = document.querySelector("#btnDecodificar");
 
 var Codigos = {
     a: "ai",
@@ -40,4 +41,37 @@ function Codificador(texto){
 btnCodificar.addEventListener("click", function(){
     var textoCodificado = Codificador(textoEntrada.value);
     pSaida.innerHTML = textoCodificado;
+});
+
+function Decodificador(texto) {
+    
+    var textoDecodificado = String(texto).replace(/ai|enter|imes|ober|ufat/gi, function (codigo) {
+        
+        switch (codigo) {
+            case Codigos.a:
+                return "a";
+  
+            case Codigos.e:
+                return "e";
+      
+            case Codigos.i:
+               return "i";
+            
+            case Codigos.o:    
+                return "o";
+
+            case Codigos.u:
+                return "u";
+        
+            default:
+                break;
+        }
+    });
+    
+    return textoDecodificado;
+}
+
+btnDecodificar.addEventListener("click", function() {
+    var textoDecodificado = Decodificador(textoEntrada.value);
+    pSaida.innerHTML = textoDecodificado;
 });
